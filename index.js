c4743e8196e160d3eef352559384551f03630a8c
irriz-jayy <jayfelix789@gmail.com>
2022-11-22 09:21:50 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  //if less is less than 400 feet
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  if (feet > 2500) {
+    return "No can do.";
+  } else if (feet > 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  // Uses ternary conditionals to check the city
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  // Uses switch conditionals to return tips
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+      break;
+  }
 }
